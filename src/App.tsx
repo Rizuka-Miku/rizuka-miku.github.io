47e5cfda8bdadb3b1516b209c22a87349a781526
Mohamad Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-01-01 13:03:31 +0700

    feat: update mobile version to have slider

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Modal from '@/components/Modal'
 import BioData from './components/BioData'
 import oriSong from '@/db/originalSong.json'
 import ScrollToTop from '@/components/ScrollToTop'
+import CustomSlider from '@/components/CustomSlider'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
   const aboutRef = useRef<HTMLDivElement>(null)
   const oriRef = useRef<HTMLDivElement>(null)
   const coverRef = useRef<HTMLDivElement>(null)
+  const isMobile = window.innerWidth < 768
 
   const [selectedSong, setSelectedSong] = useState<typeof oriSong[0] | null>(null);
 
@@ -48,11 +50,21 @@ function App() {
         <section id="project" className="w-full py-12 md:py-24 lg:py-32 bg-muted" ref={oriRef}>
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-8">Original Song</h2>
-            <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-              {oriSong?.map((song,index) => (
-                <ProjectCard imageLink={song.imageLink} title={song.title} description={song.description} projectLink={song.projectLink}  key={index} lyrics={song.lyrics} onViewLyrics={() => setSelectedSong(song)}/>
-              ))}
-            </div>
+            {isMobile ? (
+              <>
+                <CustomSlider slidesLength={oriSong.length}>
+                  {oriSong.map((song,index) => (
+                    <ProjectCard imageLink={song.imageLink} title={song.title} description={song.description} projectLink={song.projectLink} key={index} lyrics={song.lyrics} onViewLyrics={() => setSelectedSong(song)}/>
+                  ))}
+                  </CustomSlider >
+              </>
+            ) : (
+              <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {oriSong?.map((song,index) => (
+                  <ProjectCard imageLink={song.imageLink} title={song.title} description={song.description} projectLink={song.projectLink}  key={index} lyrics={song.lyrics} onViewLyrics={() => setSelectedSong(song)}/>
+                ))}
+              </div>
+            )}
           </div>
           <Modal
              isOpen={!!selectedSong}
@@ -72,13 +84,24 @@ function App() {
         <section id="project" className="w-full py-12 md:py-24 lg:py-32 bg-muted" ref={coverRef}>
           <div className="container px-4 md:px-6">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-8">Cover Song</h2>
-            <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-              {/* <ProjectCard imageLink='https://img.youtube.com/vi/xawGBRZn9VQ/hqdefault.jpg' title='Itoshisha no Defense' description='Itoshisha no Defense by JKT48' projectLink='https://www.youtube.com/watch?v=xawGBRZn9VQ'/> */}
-              {/* <ProjectCard imageLink='https://img.youtube.com/vi/5729CtVSFGc/hqdefault.jpg' title='BINGO!' description='BINGO! by JKT48' projectLink='https://www.youtube.com/watch?v=5729CtVSFGc'/> */}
-              <ProjectCard imageLink='https://img.youtube.com/vi/GRQ14yZh3KY/hqdefault.jpg' title='恋色' description='恋色 by もさを' projectLink='https://www.youtube.com/watch?v=GRQ14yZh3KY'/>
-              {/* <ProjectCard imageLink='https://img.youtube.com/vi/gEX1Hr5lmU8/hqdefault.jpg' title='Seventeen (Cover by Rizuka Miku & Vhika Hikari)' description='Seventeen by JKT48' projectLink='https://www.youtube.com/watch?v=gEX1Hr5lmU8'/> */}
-              <ProjectCard imageLink='https://img.youtube.com/vi/CG9sYT70xHw/hqdefault.jpg' title='Heart Gata Virus (Cover by Rizuka Miku, Vhika Hikari, Mariela Riona)' description='Seventeen by JKT48' projectLink='https://www.youtube.com/watch?v=CG9sYT70xHw'/>
-            </div>
+            {isMobile ? 
+              (
+                <>
+                  <CustomSlider slidesLength={2}>
+                    <ProjectCard imageLink='https://img.youtube.com/vi/CG9sYT70xHw/hqdefault.jpg' title='Heart Gata Virus (Cover by Rizuka Miku, Vhika Hikari, Mariela Riona)' description='Seventeen by JKT48' projectLink='https://www.youtube.com/watch?v=CG9sYT70xHw'/>
+                    <ProjectCard imageLink='https://img.youtube.com/vi/GRQ14yZh3KY/hqdefault.jpg' title='恋色' description='恋色 by もさを' projectLink='https://www.youtube.com/watch?v=GRQ14yZh3KY'/>
+                  </CustomSlider>
+                </>
+              )
+            : (
+              <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {/* <ProjectCard imageLink='https://img.youtube.com/vi/xawGBRZn9VQ/hqdefault.jpg' title='Itoshisha no Defense' description='Itoshisha no Defense by JKT48' projectLink='https://www.youtube.com/watch?v=xawGBRZn9VQ'/> */}
+                {/* <ProjectCard imageLink='https://img.youtube.com/vi/5729CtVSFGc/hqdefault.jpg' title='BINGO!' description='BINGO! by JKT48' projectLink='https://www.youtube.com/watch?v=5729CtVSFGc'/> */}
+                <ProjectCard imageLink='https://img.youtube.com/vi/GRQ14yZh3KY/hqdefault.jpg' title='恋色' description='恋色 by もさを' projectLink='https://www.youtube.com/watch?v=GRQ14yZh3KY'/>
+                {/* <ProjectCard imageLink='https://img.youtube.com/vi/gEX1Hr5lmU8/hqdefault.jpg' title='Seventeen (Cover by Rizuka Miku & Vhika Hikari)' description='Seventeen by JKT48' projectLink='https://www.youtube.com/watch?v=gEX1Hr5lmU8'/> */}
+                <ProjectCard imageLink='https://img.youtube.com/vi/CG9sYT70xHw/hqdefault.jpg' title='Heart Gata Virus (Cover by Rizuka Miku, Vhika Hikari, Mariela Riona)' description='Seventeen by JKT48' projectLink='https://www.youtube.com/watch?v=CG9sYT70xHw'/>
+              </div>
+            )}
           </div>
         </section>
         <ScrollToTop />
---
+++ src/components/CustomSlider.tsx
@@ -0,0 +1,46 @@
+import { useState, ReactNode } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+interface SliderProps {
+    slidesLength: number;
+    children: ReactNode;
+  }
+
+const CustomSlider = ({ slidesLength, children }: SliderProps) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const calculateThumbPosition = () => (activeIndex / (slidesLength - 1)) * 100;
+
+//   console.log('thumbWidth', thumbWidth);
+  console.log('calculateThumbPosition', calculateThumbPosition());
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange: (_: number, next: number) => setActiveIndex(next),
+    appendDots: () => (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div className="w-1/5 h-2 bg-[#A1326F] rounded overflow-hidden my-2">
+          <div
+            className="h-full bg-slate-200 rounded transition-all duration-500 ease-in-out"
+            style={{
+              width: `100%`,
+              transform: `translateX(${calculateThumbPosition()}%)`,
+            }}
+          />
+        </div>
+      </div>
+    ),
+    arrows: false,
+    adaptiveHeight: true,
+  };
+
+  return <Slider {...settings}>{children}</Slider>;
+};
+
+export default CustomSlider;
