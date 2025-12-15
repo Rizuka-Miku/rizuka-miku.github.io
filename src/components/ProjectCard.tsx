ee8d0d74fad0dab8f1ad094f3828fd0944f12430
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-12-15 10:09:39 +0700

    feat: add dark mode

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -23,14 +23,14 @@ const ProjectCard = ({imageLink, title, description, projectLink, lyrics, onView
                 />
             </CardHeader>
             <CardContent className="space-y-2">
-                <h3 className="text-xl font-bold">{title}</h3>
-                    <p className="text-muted-foreground">
+                <h3 className="text-xl font-bold text-black dark:text-white">{title}</h3>
+                    <p className="text-muted-foreground text-black dark:text-gray-300">
                         {description} 
                         <div className="flex items-center justify-start gap-3 mt-3">
-                            <p><a className="text-[#A1326F]" href={`${projectLink}`}>Go to</a></p>
+                            <p><a className="text-[#A1326F] dark:text-pink-400" href={`${projectLink}`}>Go to</a></p>
                             <Button 
                                 onClick={onViewLyrics} 
-                                className={`${lyrics && lyrics !== "" ? "" : "hidden"} bg-[#A1326F] hover:bg-[#8B2A5E] text-white duration-500`}
+                                className={`${lyrics && lyrics !== "" ? "" : "hidden"} bg-[#A1326F] hover:bg-[#8B2A5E] text-white duration-500 dark:bg-pink-600 dark:hover:bg-pink-700`}
                             >
                                 View Lyrics
                             </Button>
