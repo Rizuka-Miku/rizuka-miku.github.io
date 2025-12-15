7512950074c6f99a887394d62031ab3983911e8a
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-12-15 10:54:31 +0700

    fix: fix dark mode

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,11 +12,11 @@ const Modal = ({ isOpen, onClose, title, children }: ModalProps) => {
 
   return (
     <div className="fixed inset-0 z-[55] flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative bg-white rounded-lg shadow-lg w-11/12 max-w-md max-h-[70vh] overflow-y-auto">
-        <div className="sticky top-0 bg-white z-50 border-b px-6 py-4 flex justify-between items-center">
-          <h3 className="text-lg font-semibold">{title}</h3>
+      <div className="relative bg-white dark:bg-black rounded-lg shadow-lg w-11/12 max-w-md max-h-[70vh] overflow-y-auto">
+        <div className="sticky top-0 bg-white dark:bg-black z-50 border-b px-6 py-4 flex justify-between items-center">
+          <h3 className="text-lg font-semibold text-black dark:text-white">{title}</h3>
           <button
-            className="text-gray-500 hover:text-black"
+            className="text-gray-500 hover:text-black dark:hover:text-white focus:outline-none"
             onClick={onClose}
           >
             ✖
